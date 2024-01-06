2f4898fe0d65664bb5f0ada17ee891e788f82876
Michalek007 <nmichal02@op.pl>
2024-01-06 19:47:10 +0100

    esp->main.cpp: added connectToWiFi, mma8452Init functions, updated variables naming, deleted unnecessary delays

--- esp/src/main.cpp
+++ esp/src/main.cpp
@@ -7,20 +7,25 @@
 
 #include "hcsr04.h"
 
-#define Addr 0x1C
+#define addr 0x1C
+
+// put function declarations here:
+void connectToWiFi(const char*, const char*);
+void mma8452Init();
 
 const char* ssid     = "DESKTOP-O2OLJ1H 8056";         // The SSID (name) of the Wi-Fi network you want to connect to
 const char* password = "L5w7393^";     // The password of the Wi-Fi network
-String url_get_action = "http://192.168.1.22:5000/get_action/";
-String url_add_acc = "http://192.168.1.22:5000/add_acc/";
 WiFiClient client;
 HTTPClient http;
+String servername = "http://192.168.97.253:5000";
+String urlGetAction = servername + "/get_action/";
+String urlAddAcc = servername + "/add_acc/";
 
 void setup() {
 
   pinMode(trigger_pin, OUTPUT);
   pinMode(echo_pin, INPUT);
-  
+
   // Initialise SPI communication as MASTER
   SPI.begin();
   // Initialise I2C communication as MASTER
@@ -30,55 +35,15 @@ void setup() {
   delay(10);
   Serial.println('\n');
 
-   // Start I2C Transmission
-  Wire.beginTransmission(Addr);
-  // Select control register
-  Wire.write(0x2A);
-  // StandBy mode
-  Wire.write((byte)0x00);
-  // Stop I2C Transmission
-  Wire.endTransmission();
-
-  // Start I2C Transmission
-  Wire.beginTransmission(Addr);
-  // Select control register
-  Wire.write(0x2A);
-  // Active mode
-  Wire.write(0x01);
-  // Stop I2C Transmission
-  Wire.endTransmission();
-
-  // Start I2C Transmission
-  Wire.beginTransmission(Addr);
-  // Select control register
-  Wire.write(0x0E);
-  // Set range to +/- 2g
-  Wire.write((byte)0x00);
-  // Stop I2C Transmission
-  Wire.endTransmission();
-  delay(300);
-  
-  WiFi.begin(ssid, password);             // Connect to the network
-  Serial.print("Connecting to ");
-  Serial.print(ssid); Serial.println(" ...");
-
-  int i = 0;
-  while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
-    delay(1000);
-    Serial.print(++i); Serial.print(' ');
-  }
-
-  Serial.println('\n');
-  Serial.println("Connection established!");  
-  Serial.print("IP address:\t");
-  Serial.println(WiFi.localIP());         // Send the IP address of the ESP8266 to the computer
+  mma8452Init();
+  connectToWiFi(ssid, password);
 }
 
 void loop() {
 unsigned int data[7];
 
   // Request 7 bytes of data
-  Wire.requestFrom(Addr, 7);
+  Wire.requestFrom(addr, 7);
   
   // Read 7 bytes of data
   // staus, xAccl lsb, xAccl msb, yAccl lsb, yAccl msb, zAccl lsb, zAccl msb
@@ -120,8 +85,8 @@ unsigned int data[7];
   Serial.print("Acceleration in Z-Axis : ");
   Serial.println(zAccl);
 
-  String addAcc = url_add_acc + "?x_axis=" + xAccl + "&y_axis=" + yAccl + "&z_axis=" + zAccl;
-  String getAction = url_get_action;
+  String addAcc = urlAddAcc + "?x_axis=" + xAccl + "&y_axis=" + yAccl + "&z_axis=" + zAccl;
+  String getAction = urlGetAction;
 
   // server authentication, insert user and password below
   //http.setAuthorization("REPLACE_WITH_SERVER_USERNAME", "REPLACE_WITH_SERVER_PASSWORD");
@@ -149,8 +114,48 @@ unsigned int data[7];
 
   // getting values from hcsr04
   // float dist = check_distance();
-  // Serial.println(dist);
+  // Serial.println(dist);  
+}
 
-  delay(100);
-  
+// put function definitions here:
+void connectToWiFi(const char* ssid, const char* password) {
+  WiFi.begin(ssid, password);
+   Serial.println("Connecting");
+   while(WiFi.status() != WL_CONNECTED) {
+     delay(500);
+     Serial.print(".");
+   }
+   Serial.println("");
+   Serial.print("Connected to WiFi network with IP Address: ");
+   Serial.println(WiFi.localIP());
+}
+
+void mma8452Init(){
+   // Start I2C Transmission
+  Wire.beginTransmission(addr);
+  // Select control register
+  Wire.write(0x2A);
+  // StandBy mode
+  Wire.write((byte)0x00);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+
+  // Start I2C Transmission
+  Wire.beginTransmission(addr);
+  // Select control register
+  Wire.write(0x2A);
+  // Active mode
+  Wire.write(0x01);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+
+  // Start I2C Transmission
+  Wire.beginTransmission(addr);
+  // Select control register
+  Wire.write(0x0E);
+  // Set range to +/- 2g
+  Wire.write((byte)0x00);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+  delay(300);
 }
