312c3cbf434659be25966f85dc66b298444b2aae
Michalek007 <nmichal02@op.pl>
2024-01-11 22:45:03 +0100

    Added: views (data_views.py) and templates files (table_base.html, acc/velocity/position_table.html) for displaying acc, velocity & position data in tables, updated: main.css, base.html

--- api/app/data_views.py
+++ api/app/data_views.py
@@ -14,3 +14,16 @@ def data():
     return render_template("data.html")
 
 
+@app.route('/acc_table/', methods=['GET'])
+def acc_table():
+    return render_template('acc_table.html')
+
+
+@app.route('/velocity_table/', methods=['GET'])
+def velocity_table():
+    return render_template('velocity_table.html')
+
+
+@app.route('/position_table/', methods=['GET'])
+def position_table():
+    return render_template('position_table.html')
